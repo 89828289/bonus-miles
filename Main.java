2df9ff983fef291518a114948ef4e3681f411433
Elena <manyhka67@mail.ru>
2022-10-28 18:35:35 +0300

    add Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 2000;
+        int mileCost = 20;
+
+        int milesTotal = ticketPrice/mileCost;
+        System.out.println("количество бонусных миль" + milesTotal);
+    }
+}
